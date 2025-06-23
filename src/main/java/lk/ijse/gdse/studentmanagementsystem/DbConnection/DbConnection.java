08dc7e626481ccbb9da3bbc4bfe778441acfc8a9
ErangaFernando404 <erangafdo1994@gmail.com>
2025-06-23 11:41:47 +0530

    create DbConnection

---
+++ src/main/java/lk/ijse/gdse/studentmanagementsystem/DbConnection/DbConnection.java
@@ -0,0 +1,29 @@
+package lk.ijse.gdse.studentmanagementsystem.DbConnection;
+
+import lombok.Getter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Getter
+public class DbConnection {
+    private static DbConnection dbConnection;
+    private Connection connection;
+
+    private DbConnection() throws ClassNotFoundException, SQLException {
+         Class.forName("com.mysql.cj.jdbc.Driver");
+          connection =DriverManager.getConnection("jdbc:mysql://localhost:3306/self01", "root", "user1");
+    }
+
+    public static DbConnection getInstance() throws ClassNotFoundException, SQLException {
+        if (dbConnection == null) {
+            dbConnection = new DbConnection();
+        }
+        return dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
