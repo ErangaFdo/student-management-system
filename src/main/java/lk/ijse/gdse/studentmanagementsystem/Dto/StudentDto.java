75182420c2874741c4198e360e683a9c0b9694f1
ErangaFernando404 <erangafdo1994@gmail.com>
2025-06-23 11:51:28 +0530

    create StudentDto

---
+++ src/main/java/lk/ijse/gdse/studentmanagementsystem/Dto/StudentDto.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse.studentmanagementsystem.Dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentDto {
+    private String StudentId;
+    private String StudentName;
+    private String StudentAddress;
+    private String StudentBdy;
+    private String StudentAge;
+    private String StudentPhoneNumber;
+
+
+
+
+}
