7374634376a3898c067e69ada24c4e5a01f05dfb
ErangaFernando404 <erangafdo1994@gmail.com>
2025-06-23 11:51:50 +0530

    create StudentTm

---
+++ src/main/java/lk/ijse/gdse/studentmanagementsystem/Dto/Tm/StudentTm.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse.studentmanagementsystem.Dto.Tm;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentTm {
+    private String StudentId;
+    private String StudentName;
+    private String StudentAddress;
+    private String StudentBdy;
+    private String StudentAge;
+    private String StudentPhoneNumber;
+
+
+
+
+}
