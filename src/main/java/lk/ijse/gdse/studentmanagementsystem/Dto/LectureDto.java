c22e7cb6053eb060e5df793c271c078bd19eb489
ErangaFernando404 <erangafdo1994@gmail.com>
2025-06-28 16:44:49 +0530

    Lecture Dto

---
+++ src/main/java/lk/ijse/gdse/studentmanagementsystem/Dto/LectureDto.java
@@ -0,0 +1,19 @@
+package lk.ijse.gdse.studentmanagementsystem.Dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class LectureDto {
+    private String lectureId;
+    private String lectureName;
+    private String lectureAge;
+    private String lectureAdress;
+    private String lecturePhone;
+    private String CouresId;
+
+}
